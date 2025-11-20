62a6e1ba9fe47d87beff93522fbfdc09be421150
mhoonkim <mhoonkim@smilegate.com>
2025-11-20 19:54:09 +0900

    변경없음

--- terraform/states.tf
+++ terraform/states.tf
@@ -6,3 +6,4 @@ resource "aws_sfn_state_machine" "aft_account_provisioning_customizations" {
   role_arn   = aws_iam_role.aft_states.arn
   definition = templatefile("${path.module}/states/customizations.asl.json", {})
 }
+
